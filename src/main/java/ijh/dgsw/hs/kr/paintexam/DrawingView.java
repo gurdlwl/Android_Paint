b6cdc69e4e7be09393d9e3ef8516f9c0384e2969
gurdlwl <gurdlwl@gmail.com>
2019-05-21 12:46:56 +0900

    [UPDATE]

--- src/main/java/ijh/dgsw/hs/kr/paintexam/DrawingView.java
+++ src/main/java/ijh/dgsw/hs/kr/paintexam/DrawingView.java
@@ -1,5 +1,6 @@
 package ijh.dgsw.hs.kr.paintexam;
 
+import android.Manifest;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -7,33 +8,56 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class DrawingView extends AppCompatImageView implements View.OnTouchListener {
 
     private Paint paint;
     // private float x = 0, y = 0;
     private Path path;
+    private ArrayList<Path> paths;
+    private int color;
+    private ArrayList<Integer> colors;
 
     private void init(){
         paint = new Paint();
         paint.setColor(Color.BLACK);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeCap(Paint.Cap.ROUND);
-        paint.setStrokeWidth(15);
+        paint.setStrokeCap(Paint.Cap.ROUND); // 모서리 둥글게
+        paint.setStrokeWidth(15); // 선 굵기
         paint.setAntiAlias(true);
+
         path = new Path();
+        paths = new ArrayList<>();
+        color = Color.BLACK;
+        colors = new ArrayList<>();
+
         setOnTouchListener(this);
+    }
+
+    public void setColor(int color){
+        path = new Path();
+        this.color = color;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        // canvas.drawColor(Color.WHITE);
 
+        for(int i = 0; i < paths.size(); i++){
+            Log.d("DV", "stored path " + i);
+            paint.setColor(colors.get(i));
+            canvas.drawPath(paths.get(i), paint);
+        }
+        paint.setColor(color);
         // canvas.drawCircle(x, y, 50, paint);
         canvas.drawPath(path, paint);
-
     }
 
     @Override
@@ -49,11 +73,24 @@ public class DrawingView extends AppCompatImageView implements View.OnTouchListe
                 // y = event.getY();
                 path.lineTo(event.getX(), event.getY());
                 break;
-            case MotionEvent.ACTION_UP: break;
+            case MotionEvent.ACTION_UP:
+                paths.add(path);
+                colors.add(color);
+                break;
         }
 
         invalidate();
         return false;
+    }
+
+    public void setPathBack(){
+        if(paths.size() == 0)
+            return;
+
+        for(int i = paths.size(); i >= 0; i--){
+            paths.remove(i);
+            invalidate();
+        }
     }
 
     public DrawingView(Context context) {
--- src/main/java/ijh/dgsw/hs/kr/paintexam/MainActivity.java
+++ src/main/java/ijh/dgsw/hs/kr/paintexam/MainActivity.java
@@ -1,13 +1,74 @@
 package ijh.dgsw.hs.kr.paintexam;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawingView drawingView;
+    private RadioGroup colorGr;
+    private RadioButton color1;
+    private RadioButton color2;
+    private RadioButton color3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawingView = findViewById(R.id.drawingView);
+
+        color1 = findViewById(R.id.radioButton);
+        color2 = findViewById(R.id.radioButton2);
+        color3 = findViewById(R.id.radioButton3);
+
+        colorGr = findViewById(R.id.selColorGroup);
+        colorGr.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                int color = 0;
+
+                if(checkedId == color1.getId())
+                    color = R.color.color1;
+                else if(checkedId == color2.getId())
+                    color = R.color.color2;
+                else if(checkedId == color3.getId())
+                    color = R.color.color3;
+
+                drawingView.setColor(color);
+            }
+        });
     }
+
+    @Override
+    public void onBackPressed() {
+        drawingView.setPathBack();
+    }
+
+    // 파일 저장
+    /*
+    public void onSave(){
+        try{
+            drawingView.setDrawingCacheEnabled(true);
+            File file = new File("/");
+            FileOutputStream outputStream = new FileOutputStream(file);
+            Bitmap bitmap = drawingView.getDrawingCache();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+
+            Toast.makeText(this, "파일 저장 성공", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException fnfe){
+            fnfe.printStackTrace();
+            Toast.makeText(this, "파일 저장 실패", Toast.LENGTH_SHORT).show();
+        }
+    }
+    */
 }
